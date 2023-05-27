7264f877d8cbbac67a092462d633d34ea772e92c
Paul Sturtz <sturtzpaul@gmail.com>
2023-05-26 22:44:42 -0400

    Footer

--- app/layout.js
+++ app/layout.js
@@ -15,13 +15,40 @@ const RootLayout = ({children}) => {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          <div className="main">
-          </div>
+          <div className="main"></div>
 
           <main className="app">
             <Nav />
             {children}
           </main>
+          <footer className="z-50 w-full h-6 flex justify-center absolute">
+            <div className="text-primary-grey text-sm px-6 text-center">
+              Created By Paul Sturtz{' | '}
+              <a
+                target="_blank"
+                href="https://github.com/psturtz"
+                className="underline hover:text-primary-white"
+              >
+                Github
+              </a>
+              {' | '}
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/paulsturtz/"
+                className="underline hover:text-primary-white"
+              >
+                LinkedIn
+              </a>
+              {' | '}
+              <a
+                target="_blank"
+                href="https://www.termsfeed.com/live/f84ea026-71cd-4e28-a671-eaf88a49393a"
+                className="underline hover:text-primary-white"
+              >
+                Fair Use Disclaimer
+              </a>
+            </div>
+          </footer>
         </Provider>
       </body>
     </html>
